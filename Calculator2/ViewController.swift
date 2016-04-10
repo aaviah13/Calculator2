59c77e474d14b8fa9af95d220882cfdb16348675
aaviah13 <github2@aaviah.com>
2016-04-10 16:09:20 -0700

    Added operators

--- Calculator2/ViewController.swift
+++ Calculator2/ViewController.swift
@@ -32,6 +32,41 @@ class ViewController: UIViewController {
 
    var operandStack = Array<Double>()
 
+   @IBAction func operate(sender: UIButton) {
+      let operation = sender.currentTitle!
+
+      // Auto-enter when using an operator
+      if userIsInTheMiddleOfTypingANumber {
+         enter()
+      }
+
+      switch operation {
+      case "×": performOperation { $0 * $1 }
+      case "÷": performOperation { $1 / $0 }
+      case "+": performOperation { $0 + $1 }
+      case "−": performOperation { $1 - $0 }
+      case "√": performOperation { sqrt($0) }
+      default: break
+      }
+   }
+
+   func performOperation(operation: (Double, Double) -> Double) {
+      if operandStack.count >= 2 {
+         displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+         enter()
+      }
+   }
+
+   // Objective-C doesn't like overloaded functions, when using UIViewController
+   // Need to specify this is non-objective-C
+   @nonobjc
+   func performOperation(operation: Double -> Double) {
+      if operandStack.count >= 1 {
+         displayValue = operation(operandStack.removeLast())
+         enter()
+      }
+   }
+
    @IBAction func enter() {
       userIsInTheMiddleOfTypingANumber = false
       operandStack.append(displayValue)
